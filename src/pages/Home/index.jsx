fefbe19ae2435f37691aa71abda8169c65883598
Bruno Franco <11.brunofm@gmail.com>
2021-08-03 11:41:45 -0300

    Nova refatoração, URL com nome da cidade

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
@@ -9,8 +9,10 @@ import './styles.sass';
 export default function Home() {
     const history = useHistory();
 
-    const [city, setCity] = useState();
-    const { setWeather } = useWeatherContext();
+    const [city, setCity] = useState('');
+    const [location, setLocation] = useState(false);
+    
+    const { weather, setWeather } = useWeatherContext();
 
     let getWeather = async (lat, lon) => {
         let res = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
@@ -39,42 +41,57 @@ export default function Home() {
         });
         setWeather(res.data);
         console.log(res.data);
-        history.push('/clima');
+        setLocation(true);
     }
   
     function handleLocationTrue() {
         return navigator.geolocation.getCurrentPosition((position) => {
         getWeather(position.coords.latitude, position.coords.longitude);
-        history.push(`/clima`);
-        })
+        setLocation(true);
+        });
     }
 
+    useEffect(() => {
+        if(weather){
+            console.log(history, weather);
+            history.push(`/clima/${weather['name']}`);
+        }
+    }, [weather, history])
 
-    return(
-        <>
-        <div className="home-title">
-            <h1> How's the Weather? </h1>
-        </div>
-        <div className="home-content">
-            <aside>
-            <div className="aside-content">
-                <h1> Ver clima em sua localização atual </h1>
-                <button onClick={handleLocationTrue}> Ative sua localização </button>
+
+    if(location === false){
+        return(
+            <>
+            <div className="home-title">
+                <h1> How's the Weather? </h1>
             </div>
-            </aside>
-            <div className="divider">
-            <div className="line"></div>
+            <div className="home-content">
+                <aside>
+                <div className="aside-content">
+                    <h1> Ver clima em sua localização atual </h1>
+                    <button onClick={handleLocationTrue}> Ative sua localização </button>
+                </div>
+                </aside>
+                <div className="divider">
+                <div className="line"></div>
+                </div>
+                <main>
+                <div className="main-content">
+                    <h1> Ver o clima na cidade que quiser </h1>
+                    <form onSubmit={handleGetWeatherByName}>
+                    <input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Cidade (ex: Jundiaí, São Paulo, Rio de Janeiro)" />
+                    <button type="submit"> Ver </button>
+                    </form>
+                </div>
+                </main>
             </div>
-            <main>
-            <div className="main-content">
-                <h1> Ver o clima na cidade que quiser </h1>
-                <form onSubmit={handleGetWeatherByName}>
-                <input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Cidade (ex: Jundiaí, São Paulo, Rio de Janeiro)" />
-                <button type="submit"> Ver </button>
-                </form>
+            </>
+        );
+    } else {
+        return (
+            <div className="loading">
+                <h4> Loading... </h4>
             </div>
-            </main>
-        </div>
-        </>
-    );
+        );
+    }
 }
--- src/pages/Weather/index.jsx
+++ src/pages/Weather/index.jsx
@@ -15,57 +15,50 @@ import sunImg from '../../images/Sun.svg';
 export default function Weather() {
   const history = useHistory();
   
-  const {weather}  = useWeatherContext();
+  const {weather, setWeather}  = useWeatherContext();
 
-  if(weather === false){
-      return (
-          <div className="loading">
-              <h4> Loading... </h4>
-          </div>
-      );
-  } else {
-    function handleWeatherTheme() {
-        if(weather['weather'][0]['main'] === "Mist"){
-          return fogImg;
-        } else if(weather['weather'][0]['main'] === "Clear"){
-          return sunImg;
-        } else if(weather['weather'][0]['main'] === "Clouds"){
-          return cloudImg;
-        }
-      }
-  
-      function handleBack() {
-        history.push('/');
-      }
-  
-      return (
-        <div className="weather">
-          <section className="main-info">
-            <div className="city-content">
-              <h1> {weather['name']}, {weather['sys']['country']} </h1>
-              <span> {weather['weather'][0]['description']} </span>
-            </div>
-              <img src={handleWeatherTheme()} alt="Weather" />
-            <div className="temp-content">
-              <h2> {Math.round(JSON.stringify(weather['main']['temp']))}° </h2>
-              <span>Sensação: {Math.round(JSON.stringify(weather['main']['feels_like']))}° </span>
-            </div>
-            <div>
-              <span> Clique no botão da lateral direito para voltar </span>
-            </div>
-          </section>
-          <section className="back-button">
-            <button onClick={handleBack}> {'<'} </button>
-          </section>
-          <section className="other-info">
-            <div className="weather-content">
-              <Card text="Temperatura máxima do dia" value={Math.round(JSON.stringify(weather['main']['temp_max']))} type="°" />
-              <Card text="Temperatura mínima do dia" value={Math.round(JSON.stringify(weather['main']['temp_min']))} type="°" />
-              <Card text="Umidade" value={JSON.stringify(weather['main']['humidity'])} type="%" />
-              <Card text="Pressão" value={JSON.stringify(weather['main']['pressure'])} type=" hPa" />
-            </div>
-          </section>
-        </div>
-      );
+  function handleWeatherTheme() {
+    if(weather['weather'][0]['main'] === "Mist"){
+      return fogImg;
+    } else if(weather['weather'][0]['main'] === "Clear"){
+      return sunImg;
+    } else if(weather['weather'][0]['main'] === "Clouds"){
+      return cloudImg;
+    }
   }
+  
+  function handleBack() {
+    setWeather(false);
+    history.push('/');
+  }
+  
+  return (
+    <div className="weather">
+      <section className="main-info">
+        <div className="city-content">
+          <h1> {weather['name']}, {weather['sys']['country']} </h1>
+          <span> {weather['weather'][0]['description']} </span>
+        </div>
+          <img src={handleWeatherTheme()} alt="Weather" />
+        <div className="temp-content">
+          <h2> {Math.round(JSON.stringify(weather['main']['temp']))}° </h2>
+          <span>Sensação: {Math.round(JSON.stringify(weather['main']['feels_like']))}° </span>
+        </div>
+        <div>
+          <span> Clique no botão da lateral direito para voltar </span>
+        </div>
+      </section>
+      <section className="back-button">
+        <button onClick={handleBack}> {'<'} </button>
+      </section>
+      <section className="other-info">
+        <div className="weather-content">
+          <Card text="Temperatura máxima do dia" value={Math.round(JSON.stringify(weather['main']['temp_max']))} type="°" />
+          <Card text="Temperatura mínima do dia" value={Math.round(JSON.stringify(weather['main']['temp_min']))} type="°" />
+          <Card text="Umidade" value={JSON.stringify(weather['main']['humidity'])} type="%" />
+          <Card text="Pressão" value={JSON.stringify(weather['main']['pressure'])} type=" hPa" />
+        </div>
+      </section>
+    </div>
+  );
 }
